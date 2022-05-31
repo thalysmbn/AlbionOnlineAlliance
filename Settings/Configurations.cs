44cf9f7b6e13cc384844b70c45c0f2477c448606
thalysmbn <thalysmbn@gmail.com>
2022-05-31 09:03:20 -0300

    Start

---
+++ Settings/Configurations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbionOnline.Settings
+{
+    public class Configurations : IConfigurations
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string ClientToken { get; set; }
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,11 @@
+﻿using AlbionOnline.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("settings.json", false).Build();
+
+var serviceProvider = new ServiceCollection();
+serviceProvider.Configure<Configurations>(configuration.GetSection("Devlopment"));
+serviceProvider.AddLogging();
+serviceProvider.AddOptions();
